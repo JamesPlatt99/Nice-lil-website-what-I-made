03b47b083d7b5c3997ab4d4d8a65d91f062a5a26
James Platt <james.platt@neptune.com>
2018-03-08 15:30:59 +0000

    JQuery and Ajax used for dynamic page loading from the index

---
+++ Resources/JS/Menu.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("#Home").load("Pages/Home.html"); 
+    $("#About").load("Pages/About.html"); 
+    $("#GreatWebsites").load("Pages/GreatWebsites.html"); 
+    
+    $("#About").hide();
+    $("#GreatWebsites").hide();
+
+    $("#HomeLink").click(function(){
+        $("#About").slideUp();
+        $("#GreatWebsites").slideUp();
+        $("#Home").slideDown();
+    })        
+    $("#AboutLink").click(function(){
+        $("#Home").slideUp();
+        $("#GreatWebsites").slideUp();
+        $("#About").slideDown();
+    })   
+
+    $("#GreatWebsitesLink").click(function(){
+        $("#Home").slideUp();
+        $("#About").slideUp();
+        $("#GreatWebsites").slideDown();
+    })    
+});
